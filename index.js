8894593d6b303e592bb52c8219c40cf5b9fbaf74
snigbar <snigbar@outlook.com>
2023-05-20 13:54:01 +0600

    env variable added

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://akbarmhbc:sOLY0F2ujG5Y3tyd@cluster0.ldn2wkd.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ldn2wkd.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -34,8 +34,8 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("toymarket").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("toymarket").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
 
     const toysData = client.db('toymarket').collection('toys-data');
 
